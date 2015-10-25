7c529cdec28154cca96e24cd9af2cc0eb9b58ca0
Dheerendra Rathor <dheeru.rathor14@gmail.com>
2015-10-25 13:09:43 +0530

    corrected credentials

--- oauth/credentials.py
+++ oauth/credentials.py
@@ -1,5 +1,5 @@
-client_id = 'OB2565s95qB54OoS5hOtnRcFtvenHfonPG9g0Fbq'
+client_id = 'KOqhggnyUeMNognvAyuybROJ1U12AxSKPVABMUpo'
 redirect_uri = 'http://localhost:14396'
-BASE_URL = 'http://localhost:8000/'
+BASE_URL = 'http://gymkhana.iitb.ac.in/sso/'
 API_URL = BASE_URL + 'user/api/user/'
 OAUTH_URL = BASE_URL + 'oauth/authorize/'
